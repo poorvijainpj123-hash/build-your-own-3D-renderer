905df868e4b1ef2505407c30bec672b3c6e2d52f
poorvijainpj123-hash <poorvijainpj123@gmail.com>
2026-07-27 21:24:31 +0530

    almost

---
+++ shape.js
@@ -0,0 +1,44 @@
+class Sphere {
+    constructor (center, radius, material) {
+        this.center = center;
+        this.radius = radius;
+        this.material = material;
+    }
+
+    getIntersection (ray) {
+        const cp = ray.origin.minus(this.center);
+
+        const a = ray.direction.dot(ray.direction);
+        const b = 2 * cp.dot(ray.direction);
+        const c = cp.dot(cp)- this.radius*this.radius;
+
+        const discriminant = b*b - 4*a*c;
+        if(discriminant < 0) {
+            return null;
+        }
+
+        const sqrt = Math.sqrt(discriminant);
+
+        const ts = [];
+
+        const sub = (-b - sqrt) / (2 * a);
+        if(add >= 0) {
+            ts.push(add);
+        }
+
+        const add = (-b + sqrt) / (2 * a);
+        if (add >= 0) {
+            ts.push(add);
+        }
+
+        if (ts.length == 0) {
+            return null;
+        }
+
+        return Math.min.apply(null, ts);
+    }
+
+    normalAt (point) {
+        return point.minus(this.center).normalized();
+    }
+}
---
+++ vector.js
@@ -0,0 +1,63 @@
+class Vector3 {
+    constructor(x,y,z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    scale(scalar) {
+        return new Vector3(
+            this.x * scalar,
+            this.y * scalar,
+            this.z * scalar
+        );
+    }
+
+    plus(other) {
+        return new Vector3(
+            this.x + other.x,
+            this.y + other.y,
+            this.z + other.z
+        );
+    }
+
+    minus(other) {
+        return new Vector3(
+            this.x - other.x,
+            this.y - other.y,
+            this.z - other.z
+        );
+    }
+
+    dot(other) {
+        return (
+            this.x  * other.x +
+            this.y * other.y +
+            this.z * other.z
+        );
+    }
+
+    normalized() {
+        const mag =Math.sqrt(this.dot(this));
+        return new Vector3(
+            this.x / mag,
+            this.y / mag,
+            this.z / mag
+        );
+    }
+
+    static lerp(start, end, t) {
+        return start.scale(1 - t).plus(end.scale(t));
+    }
+}
+
+class Ray {
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+    at(t) {
+        return this.origin.plus(this.direction.scale(t));
+    }
+}
